0f4761cd4648e12fca1330173afb2d5735a21b98
Dylan Burns <wolfdb21@gmail.com>
2025-10-29 13:47:28 -0400

    added solver to the board script so that we know the answer to the current sudoku.

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -12,7 +12,7 @@ public class Board : MonoBehaviour
 
     private string[] sudokus;
     private string currentSudoku;
-    private string solvedSudoku;
+    private int[,] solvedSudoku = new int[9,9];
 
     public string keyPressed { get; set; } = null;
 
@@ -60,13 +60,35 @@ public class Board : MonoBehaviour
     {
         currentSudoku = sudokus[UnityEngine.Random.Range(0, sudokus.Length)];
         currentSudoku = currentSudoku.Trim();
+        currentSudoku.Replace('.', '0');
+
+        currentSudoku = 
+            "023097600" +
+            "546018279" +
+            "000200001" +
+            "084062300" +
+            "251783900" +
+            "009051800" +
+            "012075008" +
+            "968034752" +
+            "475800063";
+
+        int curr = 0;
+
+        for (int j = 0; j < solvedSudoku.GetLength(0); j++)
+        {
+            for (int k = 0; k < solvedSudoku.GetLength(1); k++)
+            {
+                solvedSudoku[j, k] = currentSudoku[curr++] - '0';
+            }
+        }
 
         int i = 0;
         foreach (Row row in rows)
         {
             foreach (Tile tile in row.tiles)
             {
-                if (currentSudoku[i] != '.')
+                if (currentSudoku[i] != '0')
                 {
                     tile.setNumber(currentSudoku[i++].ToString());
                     tile.changeable = false;
@@ -78,5 +100,86 @@ public class Board : MonoBehaviour
                 
             }
         }
+
+        sudokuSolver(solvedSudoku, 0, 0);
+
+        string answer = "";
+
+        for (int j = 0; j < solvedSudoku.GetLength(0); j++)
+        {
+            for (int k = 0; k < solvedSudoku.GetLength(1); k++)
+            {
+                answer += solvedSudoku[j,k].ToString();
+            }
+        }
+    }
+
+    private bool isSafe(int[,] mat, int row, int col, int num)
+    {
+        for (int x = 0; x < 9; ++x)
+        {
+            if (mat[row, x] == num)
+            {
+                return false;
+            }
+        }
+
+        for (int x = 0; x < 9; x++)
+        {
+            if (mat[x, col] == num)
+            {
+                return false;
+            }
+        }
+
+        int startRow = row - (row % 3);
+        int startCol = col - (col % 3);
+
+        for (int i = 0; i < 3; i++)
+        {
+            for (int j = 0; j < 3; j++)
+            {
+                if (mat[i + startRow, j + startCol] == num)
+                {
+                    return false;
+                }
+            }
+        }
+
+        return true;
+    }
+
+    private bool sudokuSolver(int[,] mat, int row, int col)
+    {
+        if (row == 8 && col == 9)
+        {
+            return true;
+        }
+
+        if (col == 9)
+        {
+            row++;
+            col = 0;
+        }
+
+        if (mat[row, col] != 0)
+        {
+            return sudokuSolver(mat, row, col + 1);
+        }
+
+        for (int num = 1; num <= 9; num++)
+        {
+            if (isSafe(mat, row, col, num))
+            {
+                mat[row, col] = num;
+                if (sudokuSolver(mat, row, col + 1))
+                {
+                    return true;
+                }
+                mat[row, col] = 0;
+            }
+        }
+
+        return false;
     }
 }
